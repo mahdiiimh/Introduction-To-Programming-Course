ff178bad5f0c4cd3619910b918d04c17bfe9ff5a
Mahdi Mahdiani <mahdimahdiani@ymail.com>
2026-09-10 02:00:25 +0330

    first program

---
+++ 01_01_Hello_world.c
@@ -0,0 +1,24 @@
+// A first program in C
+#include <stdio.h>
+
+// this is one line comment
+
+/*
+ This is 
+ Multiple
+ Line 
+ Comments
+*/
+
+
+// funtion main begins program execution
+
+// How to use printf() and usage of escape sequences
+int main(void){
+    printf("Hello World!\n");
+    printf("C is nice!\n");
+    printf("Ali\" said\n");
+    printf("ali\\ 2\n");
+    printf("a\tb\n");
+    return 0;
+}
